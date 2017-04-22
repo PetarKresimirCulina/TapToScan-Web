d8139278b6c045bd2aacd9c88fa2d8d2dd025634
petar@taptoscan.com <petar@taptoscan.com>
2017-04-22 20:06:07 +0200

    Rename snappy.cfg

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf-i386',
+        'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltoimage-i386',
+        'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
