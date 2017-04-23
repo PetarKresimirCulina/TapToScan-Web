fe5b7c1cbedf22ee50577fce9bdac93241e811ed
petar@taptoscan.com <petar@taptoscan.com>
2017-04-23 17:52:50 +0200

    Webhook test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,6 +14,8 @@ use Validator;
 use Response;
 use Auth;
 
+use Braintree_WebhookNotification;
+
 /**
      * API Controller
      */
@@ -164,6 +166,21 @@ class ApiController extends Controller
 	}
 	
 	public function handleBraintreeWebhook(Request $request) {
-		
+		if(
+			isset($_POST["bt_signature"]) &&
+			isset($_POST["bt_payload"])
+		) {
+			$webhookNotification = Braintree_WebhookNotification::parse(
+				$_POST["bt_signature"], $_POST["bt_payload"]
+			);
+
+			$message =
+				"[Webhook Received " 
+				. $webhookNotification->timestamp->format('Y-m-d H:i:s') . "] "
+				. "Kind: " . $webhookNotification->kind . " | "
+				. "Subscription: " . $webhookNotification->subscription->id . "\n";
+
+			file_put_contents("/tmp/webhook.log", $message, FILE_APPEND);
+		}
 	}
 }
