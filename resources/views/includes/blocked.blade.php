ea526ff62b1daa334af57496804c5d2e02f5033b
petar@taptoscan.com <petar@taptoscan.com>
2017-04-27 19:18:30 +0200

    Alert dialog change

--- resources/views/includes/blocked.blade.php
+++ resources/views/includes/blocked.blade.php
@@ -1,5 +1,5 @@
 <div class="flash-message">
 	@if(Auth::user()->blocked == 1)
-		<p class="alert alert-danger text-center">@lang('dashboardBilling.userBlocked') <a href="{{ route('dashboard.billing', App::getLocale()) }}">@lang('dashboardBilling.title')</a></p>
+		<p class="alert alert-warning text-center">@lang('dashboardBilling.userBlocked') <a href="{{ route('dashboard.billing', App::getLocale()) }}">@lang('dashboardBilling.title')</a></p>
 	@endif
 </div>
