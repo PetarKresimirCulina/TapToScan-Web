f4fa3e86bfaa98c0d52692b0bd657a1b03f70f66
petar@taptoscan.com <petar@taptoscan.com>
2017-04-27 18:46:38 +0200

    Dodane transakcije u HRK

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::group(['prefix' => '{lang?}', 'middleware' => 'localize'], function () {
 		Route::get('/billing/plans', 'HomeController@billingChangePlanDisplayAll')->name('dashboard.billing.changePlan');
 		Route::post('/billing/plans/change', 'HomeController@billingChangePlan')->name('dashboard.billing.changePlanRequest');
 		/* retry payment in subscription */
-		Route::get('/billing/plans/charge', 'PaymentController@billingRetryCharge')->name('dashboard.billing.retryCharge');
+		Route::get('/billing/plans/charge', 'PaymentsController@billingRetryCharge')->name('dashboard.billing.retryCharge');
 		/* invoices */
 		Route::post('/billing/invoice', 'HomeController@billingInvoice')->name('dashboard.billing.invoice');
 
